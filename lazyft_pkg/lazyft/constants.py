f844cc8e18d10401e5432814ed813fa68be1f1f0
raphael <rtnanje@gmail.com>
2021-08-07 21:55:17 -0400

    Rename constants.py to paths.py

--- lazyft_pkg/lazyft/constants.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-SCRIPT_DIRECTORY = pathlib.Path(__file__).parent.resolve()
-BASE_DIR = pathlib.Path(SCRIPT_DIRECTORY, '../../').resolve()
-CONFIG_DIR = pathlib.Path(BASE_DIR, 'configs').resolve()
-USER_DATA_DIR = pathlib.Path(BASE_DIR, 'user_data')
-STRATEGY_DIR = pathlib.Path(USER_DATA_DIR, 'strategies')
-ID_TO_LOAD_FILE = pathlib.Path(STRATEGY_DIR, 'strategy_ids.json')
-PARAMS_FILE = BASE_DIR.joinpath("lazy_params.json")
---
+++ lazyft_pkg/lazyft/paths.py
@@ -0,0 +1,13 @@
+import pathlib
+
+SCRIPT_DIRECTORY = pathlib.Path(__file__).parent.resolve()
+BASE_DIR = SCRIPT_DIRECTORY.joinpath('../../').resolve()
+
+PARAMS_FILE = BASE_DIR.joinpath("lazy_params.json")
+PARAMS_DIR = BASE_DIR.joinpath('saved_params')
+PARAMS_DIR.mkdir(exist_ok=True)
+BACKTEST_RESULTS_FILE = BASE_DIR.joinpath('backtest_results.json')
+CONFIG_DIR = BASE_DIR.joinpath('configs')
+USER_DATA_DIR = BASE_DIR.joinpath('user_data')
+STRATEGY_DIR = USER_DATA_DIR.joinpath('strategies')
+ID_TO_LOAD_FILE = STRATEGY_DIR.joinpath('strategy_ids.json')

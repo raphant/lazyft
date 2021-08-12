d3df405fdc77919a08b4f4b3d5b70d8630e135aa
raphael <rtnanje@gmail.com>
2021-08-11 23:25:17 -0400

    update LAST_HYPEROPT_RESULTS_FILE in paths.py

--- lazyft_pkg/lazyft/paths.py
+++ lazyft_pkg/lazyft/paths.py
@@ -12,3 +12,6 @@ CONFIG_DIR = BASE_DIR.joinpath('configs')
 USER_DATA_DIR = BASE_DIR.joinpath('user_data')
 STRATEGY_DIR = USER_DATA_DIR.joinpath('strategies')
 ID_TO_LOAD_FILE = STRATEGY_DIR.joinpath('strategy_ids.json')
+LAST_HYPEROPT_RESULTS_FILE = USER_DATA_DIR.joinpath(
+    'hyperopt_results', '.last_result.json'
+)

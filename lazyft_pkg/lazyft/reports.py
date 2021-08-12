5c88633ff841ebd2d34ee1996f41e8028acc0d40
raphael <rtnanje@gmail.com>
2021-08-11 23:26:29 -0400

    Remove unused classes and pull members of RepoExplorer subclasses up
    Fix head and tail not returning self issue

--- lazyft_pkg/lazyft/reports.py
+++ lazyft_pkg/lazyft/reports.py
@@ -2,7 +2,7 @@ from abc import ABCMeta, abstractmethod
 from collections import UserList
 from pathlib import Path
 from pprint import pprint
-from typing import Optional, Union, Callable
+from typing import Optional, Union, Callable, Type
 
 import pandas as pd
 import rapidjson
@@ -61,14 +61,37 @@ class Parameter:
         return data
 
 
+def get_ppd(r):
+    if r.performance.profit == 0:
+        return 0
+    ppd = (
+        r.performance.profit / (r.performance.end_date - r.performance.start_date).days
+    )
+    return ppd
+
+
 class RepoExplorer(UserList[Union[BacktestReport, HyperoptReport]], metaclass=ABCMeta):
-    def __init__(self) -> None:
+    def __init__(
+        self, file: Path, repo: Union[Type[BacktestRepo], Type[HyperoptRepo]]
+    ) -> None:
         super().__init__()
+        self._repo = repo
+        self._data_file = file
         self.reset()
 
-    @abstractmethod
     def reset(self):
-        pass
+        try:
+            self.data = self._repo.parse_file(self._data_file).reports
+        except FileNotFoundError:
+            logger.error('"{}" not found', self._data_file)
+
+        return self
+
+    def get(self, uuid: str):
+        """Get the report with the uuid or the first report in the repo"""
+        if uuid:
+            return [r for r in self if r.report_id == hash][0]
+        return self.data[0]
 
     def filter(self, func: Callable[[Report], bool]):
         self.data = list(filter(func, self.data))
@@ -76,12 +99,14 @@ class RepoExplorer(UserList[Union[BacktestReport, HyperoptReport]], metaclass=AB
 
     def head(self, n: int):
         self.data = self.data[:n]
+        return self
 
     def tail(self, n: int):
         self.data = self.data[-n:]
+        return self
 
     def sort_by_date(self, reverse=False):
-        self.data = sorted(self.data, key=lambda r: r.date, reverse=reverse)
+        self.data = sorted(self.data, key=lambda r: r.date, reverse=not reverse)
         return self
 
     def sort_by_profit(self, reverse=False):
@@ -93,15 +118,6 @@ class RepoExplorer(UserList[Union[BacktestReport, HyperoptReport]], metaclass=AB
         return self
 
     def sort_by_profit_per_day(self, reverse=False):
-        def get_ppd(r):
-            if r.performance.profit == 0:
-                return 0
-            ppd = (
-                r.performance.profit
-                / (r.performance.end_date - r.performance.start_date).days
-            )
-            return ppd
-
         self.data = sorted(self.data, key=lambda r: get_ppd(r), reverse=not reverse)
         return self
 
@@ -139,6 +155,7 @@ class RepoExplorer(UserList[Union[BacktestReport, HyperoptReport]], metaclass=AB
                 id=r.id,
                 exchange=r.exchange,
                 **r.balance_info.dict(),
+                ppd=get_ppd(r),
                 **r.performance.dict(),
                 days=(r.performance.end_date - r.performance.start_date).days
             )
@@ -151,15 +168,6 @@ class RepoExplorer(UserList[Union[BacktestReport, HyperoptReport]], metaclass=AB
 
 
 class BacktestRepoExplorer(RepoExplorer, UserList[BacktestReport]):
-    def reset(self):
-        try:
-            reports = BacktestRepo.parse_file(BACKTEST_RESULTS_FILE).reports
-        except FileNotFoundError:
-            pass
-        else:
-            self.extend(reports)
-        return self
-
     def get_hashes(self):
         return [s.hash for s in self]
 
@@ -167,115 +175,11 @@ class BacktestRepoExplorer(RepoExplorer, UserList[BacktestReport]):
         return [r for r in self if r.hash == hash].pop()
 
 
-class HyperoptRepoExplorer(RepoExplorer, UserList[HyperoptReport]):
-    def reset(self):
-        try:
-            reports = HyperoptRepo.parse_file(PARAMS_FILE).reports
-        except FileNotFoundError:
-            pass
-        else:
-            self.extend(reports)
-        return self
-
-    def by_profitable(self):
-        self.data = [r for r in self if r.performance.tot_profit > 0]
-
-
-class BacktestReportBrowser:
-    @classmethod
-    def data(cls):
-        if not BACKTEST_RESULTS_FILE.exists():
-            return {}
-        return rapidjson.loads(BACKTEST_RESULTS_FILE.read_text())
-
-    @classmethod
-    def get_hashes(cls, strategy: str):
-        return [p.get('hash') for p in cls.data().get(strategy, {})]
-
-    @classmethod
-    def get_backtest_results(cls, strategy: str):
-        if strategy not in cls.data():
-            raise KeyError(
-                'Strategy "%s" not found in %s' % (strategy, BACKTEST_RESULTS_FILE.name)
-            )
-        performances = []
-        for p in cls.data()[strategy]:
-            data = {'id': p.get('id'), 'exchange': p.get('exchange')}
-            data.update(p['performance'])
-            data.update(
-                {
-                    'start_date': p.get('start_date'),
-                    'end_date': p.get('end_date'),
-                }
-            )
-            performances.append(data)
-        df = pd.DataFrame(performances)
-        return df
-
-    @classmethod
-    def get_all_backtest_results(cls):
-        frames = []
-        for s in cls.data():
-            df = cls.get_backtest_results(s)
-            df.insert(2, 'strategy', s)
-            df.strategy = df.strategy.apply(
-                lambda x: ''.join([st for st in x if (st.isupper() | st.isdigit())])
-            )
-            frames.append(df)
-        return pd.concat(frames)
-
-    @classmethod
-    def get_backtest_result_from_id(cls, strategy: str, id: str):
-        if strategy not in cls.data() or id not in [strategy]:
-            raise KeyError(
-                'Strategy "%s" or id "%s" not found in %s'
-                % (strategy, id, BACKTEST_RESULTS_FILE.name)
-            )
-        data = [cls.data()[strategy][id]['performance']]
-        return pd.DataFrame(data)
-
-    @classmethod
-    def get_backtest_by_hash(cls, strategy: str, hash: str) -> Optional[dict]:
-        for p in cls.data()[strategy]:
-            if p.get('hash') == hash:
-                return p
-
-
-class HyperoptReportsBrowser:
-    @classmethod
-    def data(cls):
-        if not PARAMS_FILE.exists():
-            return {}
-        return rapidjson.loads(PARAMS_FILE.read_text())
-
-    @classmethod
-    def get_performances(cls, strategy: str) -> pd.DataFrame:
-        performances = []
-        assert strategy in cls.data(), "Strategy doesn't exist"
-        for id, id_dict in cls.data()[strategy].items():
-            perf = {'id': id, 'exchange': id_dict.get('exchange')}
-            perf.update(id_dict.get('balance_info', {}))
-            perf.update(id_dict['performance'])
-            perf['days'] = abs(parse(perf['from_date']) - parse(perf['to_date'])).days
-            perf.pop('to_date')
-            performances.append(perf)
-
-        df = pd.DataFrame(performances)
-        return df
-
-    @classmethod
-    def get_params(cls, strategy: str, id: str):
-        try:
-            return rapidjson.loads(
-                Path(cls.data()[strategy][id]['params_file']).read_text()
-            )
-        except KeyError:
-            raise KeyError('Could not load params for %s-%s' % (strategy, id))
-
+BacktestRepoExplorer = BacktestRepoExplorer(BACKTEST_RESULTS_FILE, BacktestRepo)
+HyperoptRepoExplorer = RepoExplorer(PARAMS_FILE, HyperoptRepo)
 
 if __name__ == '__main__':
-    report_filter = BacktestRepoExplorer()
-    pprint(report_filter.dataframe())
+    pprint(BacktestRepoExplorer.dataframe())
 
     # print('Ln70qa:')
     # pprint(ResultBrowser().get_params('BollingerBands2', 'Ln70qa'))

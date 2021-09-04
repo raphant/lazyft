3da0046401abdf5b0dab4f5fb4761fbf07e8ee8e
raphael <rtnanje@gmail.com>
2021-09-04 09:40:15 -0400

    Modify pairlist loading logic & remote unused code

--- lazyft_pkg/lazyft/backtest/commands.py
+++ lazyft_pkg/lazyft/backtest/commands.py
@@ -1,22 +1,15 @@
-import pathlib
-from typing import Optional, Union
-
-import typer
-
 from lazyft import logger, util
 from lazyft.command import Command
-from lazyft.config import Config
-from lazyft.pairlist import Pairlist
-from lazyft.parameters import GlobalParameters, CommandParameters, command_map
-from lazyft.quicktools import QuickTools
-from lazyft.strategy import Strategy
+from lazyft.command_parameters import BacktestParameters
+from lazyft.pairlist import load_pairlist_from_id
+from lazyft.reports import get_hyperopt_repo
 
 
 class BacktestCommand(Command):
     def __init__(
         self,
         strategy: str,
-        params: CommandParameters,
+        params: BacktestParameters,
         id=None,
         verbose=False,
     ) -> None:
@@ -29,63 +22,43 @@ class BacktestCommand(Command):
         self.verbose = verbose
         self.pairs = params.pairs or params.config.whitelist
         self.args = ['backtesting', f'-s {strategy}']
-        if id and not self.pairs:
-            # load pairs from ID if pairs not already provided.
-            self.pairs = Pairlist.load_from_id(strategy=strategy, id=id)
         # self.config = Strategy.init_config(config=config, strategy=strategy)
         self.secret_config = params.secrets_config
+        self._hash = ''
+        if (
+            id
+            and not self.pairs
+            and (
+                self.config['exchange']['name']
+                == get_hyperopt_repo().get_by_param_id(id).exchange
+            )
+        ):
+            # load pairs from ID if pairs not already provided.
+            self.pairs = load_pairlist_from_id(id=id)
 
     @property
     def hash(self):
         """To help avoid running the same backtest"""
         # sort for consistency
-        return util.hash(
-            ''.join(sorted(self.command_string))
+        if self._hash:
+            return self._hash
+        command_string = (
+            ''.join(sorted(self.command_string.split()))
             + self.id
             + self.config['exchange']['name']
         )
-
-
-def new_hyperopt_cli(
-    strategies: list[str] = typer.Argument(...),
-    days: int = typer.Option(None, '-d', '--days'),
-    timerange: str = typer.Option('', '-t', '--timerange'),
-    interval: str = typer.Option('5m', '-i', '--interval'),
-    config: pathlib.Path = typer.Option('config.json', '-c', '--config'),
-    verbose: bool = typer.Option(False, '-v', '--verbose'),
-):
-
-    return create_commands(
-        strategies=strategies,
-        interval=interval,
-        config=config,
-        timerange=timerange,
-        verbose=verbose,
-        days=days,
-    )
+        logger.debug('Hashing "{}"', command_string)
+        self._hash = util.hash(command_string)
+        return self._hash
 
 
 def create_commands(
-    backtest_params: CommandParameters,
+    backtest_params: BacktestParameters,
     verbose=False,
-    skip_data_download=True,
 ):
     """Create `HyperoptCommand` for each strategy in strategies."""
-    if backtest_params.secrets_config:
-        backtest_params.secrets_config = Config(backtest_params.secrets_config)
     logger.debug('Using config: {}', backtest_params.config.path)
     commands = []
-    if backtest_params.pairs:
-        backtest_params.config = backtest_params.config.tmp()
-        backtest_params.config.update_whitelist(backtest_params.pairs)
-        backtest_params.config.save()
-    if not skip_data_download:
-        QuickTools.download_data(
-            backtest_params.config,
-            interval=backtest_params.intervals_to_download,
-            days=backtest_params.days,
-            timerange=backtest_params.timerange,
-        )
     for s, id in backtest_params.strategy_id_pairs:
         command = BacktestCommand(
             s,

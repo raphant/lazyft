49b0d84a27fcba0b1484648665632c329c005d4d
raphael <rtnanje@gmail.com>
2021-09-04 09:48:06 -0400

    Simplify command_string and add download_data

--- lazyft_pkg/lazyft/command.py
+++ lazyft_pkg/lazyft/command.py
@@ -1,35 +1,28 @@
 from typing import Union
 
-from lazyft.parameters import command_map, CommandParameters, HyperoptParameters
+from lazyft.command_parameters import (
+    command_map,
+    BacktestParameters,
+    HyperoptParameters,
+)
 from lazyft.quicktools import QuickTools
 
 
 class Command:
     def __init__(
-        self, strategy, params: Union[CommandParameters, HyperoptParameters], id=None
+        self, strategy, params: Union[BacktestParameters, HyperoptParameters], id=None
     ):
         self.strategy = strategy
         self.id = id
         self.config = params.config
         self.params = params
         self.args = ['hyperopt', f'-s {strategy}']
+        self.pairs = None
 
     @property
     def command_string(self):
         params = self.params
-        assert (
-            params.days or params.timerange
-        ), "--days or --timerange must be specified"
-
         args = self.args.copy()
-        if not params.timerange:
-            hyperopt_range, backtest_range = QuickTools.get_timerange(
-                self.config, params.days, params.interval
-            )
-            if isinstance(self.params, HyperoptParameters):
-                params.timerange = hyperopt_range
-            else:
-                params.timerange = backtest_range
 
         for key, value in params.__dict__.items():
             if not value or key not in command_map or key == 'days':
@@ -41,3 +34,13 @@ class Command:
             arg_line = f"{command_map[key]} {value}".strip()
             args.append(arg_line)
         return ' '.join(args)
+
+    def download_data(self):
+        QuickTools.download_data(
+            config=self.params.config,
+            pairs=self.pairs,
+            interval=self.params.intervals_to_download,
+            days=self.params.days,
+            timerange=self.params.timerange,
+            secrets_config=self.params.secrets_config,
+        )
